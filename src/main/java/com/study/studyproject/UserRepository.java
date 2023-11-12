332be5429ef5a923940c8c71e8141cd845b55a49
naegonggae <tkdtkd97@naver.com>
2023-11-13 01:21:31 +0900

    :chore: cicd.yml 테스트

--- src/main/java/com/study/studyproject/UserRepository.java
+++ src/main/java/com/study/studyproject/UserRepository.java
@@ -2,6 +2,6 @@ package com.study.studyproject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
