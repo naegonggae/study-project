b845bb199aec33b2460e4e0543ff937122a9a7f7
naegonggae <tkdtkd97@naver.com>
2023-11-16 17:58:14 +0900

    :chore: cicd.yml 테스트

--- src/test/java/com/study/studyproject/controller/CiCdControllerTest.java
+++ src/test/java/com/study/studyproject/controller/CiCdControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CiCdControllerTest {
 
 	@Test
