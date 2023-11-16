44888f3ea8e68494f299167bcd2cc190ce016540
naegonggae <tkdtkd97@naver.com>
2023-11-16 18:24:29 +0900

    :chore: cicd.yml 테스트

--- src/test/java/com/study/studyproject/StudyProjectApplicationTests.java
+++ src/test/java/com/study/studyproject/StudyProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.study.studyproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StudyProjectApplicationTests {
 
 	@Test
